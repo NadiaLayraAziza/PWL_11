624a6272eb57310128ed67ae0aab4527455e1b7e
NadiaLayraAziza <nadialayra15@gmail.com>
2021-04-20 14:59:08 +0700

    Menambahkan  function login pada file Api/AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Request\LoginRequest;
 use App\Request\RegisterRequest;
 use App\Traits\ApiResponse;
 use App\Http\Controllers\Controller;
@@ -21,6 +22,24 @@ class AuthController extends Controller
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
+        return $this->apiSuccess([
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'user'=> $user,
+        ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+
+        if (!Auth::attempt($validated)) {
+            return $this->apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+            
         return $this->apiSuccess([
             'token' => $token,
             'token_type' => 'Bearer',
